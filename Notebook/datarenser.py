c3622ed8813b17df23dc81b764970c9cb76c0a0d
AllanChandler <cph-ac326@cphbusiness.dk>
2025-05-12 20:55:43 +0200

    Update datarenser.py

--- Notebook/datarenser.py
+++ Notebook/datarenser.py
@@ -4,42 +4,51 @@ def load_data_clean(path):
     # Læser datasættet
     df = pd.read_csv(path, index_col=0)
 
-    # Fjerner irrelevante kolonner
-    df.drop(['flight', 'departure_time', 'arrival_time'], axis=1, inplace=True)
+    # Fjerner 'flight' kolonnen, da den kun er en identifikator og ikke bidrager til analysen
+    df.drop(['flight'], axis=1, inplace=True)  
 
-    # Udfører One-Hot Encoding på de kategoriske kolonner
-    df = pd.get_dummies(df, columns=['airline', 'source_city', 'stops', 'destination_city', 'class'], dtype=pd.Int64Dtype())
+    # Fjerner 'departure_time' kolonnen, da den ikke er relevant for vores forskningsspørgsmål om dage før afrejse og antal stop
+    df.drop(['departure_time'], axis=1, inplace=True)  
+
+    # Fjerner 'arrival_time' kolonnen, da den ikke er relevant for vores forskningsspørgsmål om dage før afrejse og antal stop
+    df.drop(['arrival_time'], axis=1, inplace=True)  
+
+    # Udfører one-hot encoding på de kategoriske kolonner for at gøre dem numeriske og lettere at regressere på
+    dfNumeric = pd.get_dummies(df, columns=['airline', 'source_city', 'stops', 'destination_city', 'class'], dtype=pd.Int64Dtype())
 
     # Konverterer alle 'object'-kolonner til 'string'-type
-    for col in df:
-        if df[col].dtype == 'object':
-            df[col] = df[col].astype('string')
+    for col in dfNumeric:
+        if dfNumeric[col].dtype == 'object':
+            dfNumeric[col] = dfNumeric[col].astype('string')
 
-    return df
+    return dfNumeric
 
 def load_data_train(path):
-    # Læs datasættet
+    # Læser datasættet
     df = pd.read_csv(path)
 
-    # Fjerner irrelevante kolonner
-    df.drop(['Dep_Time'], axis=1, inplace=True)
+    # Fjerner duplikater
+    df = df.drop_duplicates()
 
-    # Konverter 'Date_of_Journey' til datetime format
-    df['Date_of_Journey'] = pd.to_datetime(df['Date_of_Journey'], format='%d/%m/%Y')
+    # Fjerner 'Dep_Time' kolonnen, da den ikke er relevant for analysen
+    df.drop(['Dep_Time'], axis=1, inplace=True) 
+    
+    # Konverterer 'Date_of_Journey' til datetime format
+    df['Date_of_Journey'] = pd.to_datetime(df['Date_of_Journey'], format='%d/%m/%Y') 
 
-    # Tilføjer 'Journey_month' og 'days_until_flight' kolonner
+    # Opretter en ny kolonne 'Journey_month' som indeholder måneden fra 'Date_of_Journey'
     df['Journey_month'] = df['Date_of_Journey'].dt.month
-    df['Journey_month'] = df['Journey_month'].astype('int64')  # Skift datatype til int64
+    df['Journey_month'] = df['Journey_month'].astype('int64')  # Skifter datatypen til int64
 
-    # Opretter 'Class' kolonne baseret på 'Airline' kolonnen
+    # Opdaterer den eksisterende 'Class' kolonne baseret på værdier i 'Airline' da den gamle kun havde 0 værdier
     df['Class'] = df['Airline'].apply(lambda x: 'Business' if 'Business' in x else ('Premium economy' if 'Premium economy' in x else 'Standard'))
 
-    # Udfører One-Hot Encoding på de kategoriske kolonner
-    df = pd.get_dummies(df, columns=['Airline', 'Source', 'Destination', 'Date_of_Journey'], dtype=pd.Int64Dtype())
-
+    # Udfører one-hot encoding på de kategoriske kolonner for at gøre dem numeriske og lettere at regressere på
+    dfNumeric = pd.get_dummies(df, columns=['Airline', 'Source', 'Destination', 'Date_of_Journey'], dtype=pd.Int64Dtype())
+    
     # Konverterer alle 'object'-kolonner til 'string'-type
-    for col in df:
-        if df[col].dtype == 'object':
-            df[col] = df[col].astype('string')
+    for col in dfNumeric:
+        if dfNumeric[col].dtype == 'object':
+            dfNumeric[col] = dfNumeric[col].astype('string')
 
-    return df
+    return dfNumeric
